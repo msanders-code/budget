9fd07c0e3dc7b599b7a6067811c6b5176ae4f733
sandemat <sandemat@oregonstate.edu>
2022-05-12 16:01:24 -0600

    Created year object to hold the incom and expense list's for each year

---
+++ year_obj.py
@@ -0,0 +1,31 @@
+# Object to represent the year of expenses and income
+
+class Year:
+
+    def __int__(self):
+        self._expenses_by_month = {
+            "01": None, "02": None, "03": None,
+            "04": None, "05": None, "06": None,
+            "07": None, "08": None, "09": None,
+            "10": None, "11": None, "12": None
+        }
+        self._income_by_month = {
+            "01": None, "02": None, "03": None,
+            "04": None, "05": None, "06": None,
+            "07": None, "08": None, "09": None,
+            "10": None, "11": None, "12": None
+        }
+
+    def get_expenses_by_month(self, month):
+        return self._expenses_by_month[month]
+
+    def get_income_by_month(self, month):
+        return self._income_by_month[month]
+
+    def add_income_list(self, month, lst):
+        self._income_by_month[month] = lst
+        return
+
+    def add_expense_list(self, month, lst):
+        self._expenses_by_month[month] = lst
+        return
