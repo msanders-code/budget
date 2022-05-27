e1f5e55e95d133bac81becaccc44888c52cc21cc
sandemat <sandemat@oregonstate.edu>
2022-05-27 10:17:02 -0600

    Changed the standard size of the app to initialize to 3/4 screen height and width and have the frame open maximized on screen. Removed the center command because it opens maximized

--- app_window.py
+++ app_window.py
@@ -1,18 +1,28 @@
 # Main window for the app
 import wx
-from wx import App, Frame
 
 
 class Application:
-    # Initializes the application
-    _app = App()
 
-    # Initializes the base app frame with default frame style
-    _window = Frame(None, 0, "Budget Tracker", size=(1000, 850))
-    _window.Centre()
+    def __init__(self):
+        # Initializes the application
+        self._app = wx.App()
 
-    # Shows the frame on the screen
-    _window.Show()
+        # Define base width and height for app window
+        self._width = int(wx.GetDisplaySize()[0] * 0.75)
+        self._height = int(wx.GetDisplaySize()[1] * 0.75)
+
+        # Initializes the base app frame with default frame style
+        self._window = wx.Frame(
+            None,
+            0,
+            "Budget Tracker",
+            size=(self._width, self._height)
+        )
+
+        # Maximize and show the app window on startup
+        self._window.Maximize()
+        self._window.Show()
 
     # Return the initialized app
     def get_app(self):
